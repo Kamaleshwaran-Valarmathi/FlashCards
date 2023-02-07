a55c596afd8113fbdc25b73eef9270f0338eb989
Kamaleshwaran-Valarmathi <124665343+Kamaleshwaran-Valarmathi@users.noreply.github.com>
2023-02-07 13:41:20 +0530

    Add files via upload

---
+++ src/Topics.js
@@ -0,0 +1,26 @@
+export const Topics = [
+  {
+    id: 1,
+    topic: 'All'
+  },
+  {
+    id: 2,
+    topic: 'Geometry'
+  },
+  {
+    id: 3,
+    topic: 'Mensuration'
+  },
+  {
+    id: 4,
+    topic: 'Algebra'
+  },
+  {
+    id: 5,
+    topic: 'GCD-LCM'
+  },
+  {
+    id: 6,
+    topic: 'Number System'
+  }
+];
