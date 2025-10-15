6426877238d03be298ecde531ed68ba512a0b2b8
Kamaleshwaran <124665343+Kamaleshwaran-Valarmathi@users.noreply.github.com>
2025-10-15 17:31:23 +0530

    Create constants.js

---
+++ js/constants.js
@@ -0,0 +1,92 @@
+export const categories = [
+    {
+        name: "Verbs",
+        cards: [
+            { key: "Come<br><br>வருவது", value: "Aa<br><br>आ" },
+            { key: "Go<br><br>போவது", value: "Jaa<br><br>जा" },
+            { key: "Do<br><br>செய்வது", value: "Kar<br><br>कर" },
+            { key: "Read<br><br>படிப்பது", value: "Pad<br><br>पढ़" },
+            { key: "Write<br><br>எழுதுவது", value: "Likh<br><br>लिख" },
+            { key: "Take<br><br>எடுப்பது", value: "Lae<br><br>ले" },
+            { key: "Give<br><br>கொடுப்பது", value: "Dhae<br><br>दे" },
+            { key: "Bring<br><br>கொண்டு வருவது", value: "Laa<br><br>ला" },
+            { key: "See<br><br>பார்ப்பது", value: "Dhek<br><br>देख" },
+            { key: "Show<br><br>காண்பிப்பது", value: "Dhika<br><br>दिखा" },
+            { key: "Eat<br><br>சாப்பிடுவது", value: "Kha<br><br>खा" },
+            { key: "Drink<br><br>குடிப்பது", value: "Pee<br><br>पी" },
+            { key: "Sit<br><br>உட்காருவது", value: "Bite<br><br>बैठ" },
+            { key: "Get Up<br><br>எழுந்திருப்பது", value: "Ut<br><br>उठ" },
+            { key: "Play<br><br>விளையாடுவது", value: "Khel<br><br>खेल" },
+            { key: "Speak<br><br>பேசுவது", value: "Bol<br><br>बोल" },
+            { key: "Say<br><br>சொல்வது", value: "Kah / Batha<br><br>कह / बता" },
+            { key: "Hear<br><br>கேட்பது", value: "Sun<br><br>सुन" },
+            { key: "Ask (for Something)<br><br>கேட்பது (வினா)", value: "Pooch<br><br>पूछ" },
+            { key: "Ask (for an Object)<br><br>கேட்பது (பொருட்கள்)", value: "Maang<br><br>मांग" },
+            { key: "Walk<br><br>நடப்பது", value: "Chal<br><br>चल" },
+            { key: "Run<br><br>ஓடுவது", value: "Dhaud<br><br>दौड़" },
+            { key: "Live<br><br>இருப்பது", value: "Rah<br><br>रह" },
+            { key: "Keep<br><br>வைப்பது", value: "Rakh<br><br>रख" },
+            { key: "Put<br><br>போடுவது", value: "Daal<br><br>डाल" },
+            { key: "Buy<br><br>வாங்குவது", value: "Kharedh<br><br>खरीद" },
+            { key: "Sell<br><br>விற்பது", value: "Bech<br><br>बेच" },
+            { key: "Sing<br><br>பாடுவது", value: "Gha<br><br>गा" },
+            { key: "Stop<br><br>நிறுத்துவது", value: "Ruk<br><br>रुक" },
+            { key: "Wait<br><br>காத்திருப்பது", value: "Inthazar Kar<br><br>इंतज़ार कर" },
+            { key: "Catch / Hold<br><br>பிடிப்பது", value: "Pakad<br><br>पकड़" },
+            { key: "Leave<br><br>விடுவது", value: "Chod<br><br>छोड़" },
+            { key: "Call<br><br>அழைப்பது", value: "Bulaa<br><br>बुला" },
+            { key: "Break<br><br>உடைப்பது", value: "Thod<br><br>तोड़" },
+            { key: "Cut<br><br>வெட்டுவது", value: "Kaat<br><br>काट" },
+            { key: "Drive<br><br>ஓட்டுவது", value: "Chalaa<br><br>चला" },
+            { key: "Earn<br><br>சம்பாதிப்பது", value: "Kamaa<br><br>कमा" },
+            { key: "Fear<br><br>பயப்படுவது", value: "Darna<br><br>डरना" },
+            { key: "Fight<br><br>சண்டையிடுவது", value: "Lad<br><br>लड़" },
+            { key: "Learn<br><br>கற்றுக்கொள்வது", value: "Seekh<br><br>सीख" },
+            { key: "Teach<br><br>கற்றுக்கொடுப்பது", value: "Sikhaa<br><br>सिखा" },
+            { key: "Open<br><br>திறப்பது", value: "Khol<br><br>खोल" },
+            { key: "Close<br><br>மூடுவது", value: "Bandh Kar<br><br>बंद कर" },
+            { key: "Sleep<br><br>தூங்குவது", value: "Soo<br><br>सो" },
+            { key: "Think<br><br>யோசிப்பது", value: "Soch<br><br>सोच" },
+            { key: "Dance<br><br>ஆடுவது", value: "Naach<br><br>नाच" },
+            { key: "Laugh<br><br>சிரிப்பது", value: "Has<br><br>हँस" },
+            { key: "Cry<br><br>அழுவது", value: "Ro<br><br>रो" },
+            { key: "Throw<br><br>எறிவது", value: "Faink<br><br>फेंक" },
+            { key: "Spread<br><br>பரப்புவது", value: "File<br><br>फैल" },
+            { key: "Bath<br><br>குளிப்பது", value: "Nahaa<br><br>नहा" },
+            { key: "Swim<br><br>நீந்துவது", value: "Thair<br><br>तैर" },
+            { key: "Fall<br><br>விழுவது", value: "Ghir<br><br>गिर" },
+            { key: "Send<br><br>அனுப்புவது", value: "Bejh<br><br>भेज" },
+            { key: "Fly<br><br>பறப்பது", value: "Ud<br><br>उड़" },
+            { key: "Roam<br><br>சுற்றுதல்", value: "Ghoom<br><br>घूम" },
+            { key: "Count<br><br>எண்ணுதல்", value: "Gin<br><br>गिन" },
+            { key: "Wear<br><br>உடுத்துதல்", value: "Pahan<br><br>पहन" },
+            { key: "Remove<br><br>வெளியேற்றுதல்", value: "Nikal<br><br>निकाल" },
+            { key: "Change<br><br>மாற்றுதல்", value: "Badhal<br><br>बदल" },
+            { key: "Understand<br><br>புரிதல்", value: "Samajh<br><br>समझ" },
+            { key: "Switch On<br><br>இயக்குதல்", value: "Chaalu Kar<br><br>चालू कर" },
+            { key: "Meet<br><br>சந்திப்பது", value: "Mil<br><br>मिल" },
+            { key: "Climb<br><br>ஏறுதல்", value: "Chad<br><br>चढ़" },
+            { key: "Get Down<br><br>இறங்குதல்", value: "Uthar<br><br>उतर" }
+        ]
+    },
+    {
+        name: "Present Tense",
+        cards: []
+    },
+    {
+        name: "Present Continuous Tense",
+        cards: []
+    },
+    {
+        name: "Past Tense",
+        cards: []
+    },
+    {
+        name: "Past Continuous Tense",
+        cards: []
+    },
+    {
+        name: "Future Tense",
+        cards: []
+    }
+];
